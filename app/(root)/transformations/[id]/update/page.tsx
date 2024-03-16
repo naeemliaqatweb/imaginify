9f551a91db7193e94d820401f3f8c42afc37a2d7
naeem <naeemliaqatweb@gmail.com>
2024-03-17 01:49:29 +0500

    initial commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UpadteTransformationPage() {
+  return (
+    <div>UpadteTransformationPage</div>
+  )
+}
+
+export default UpadteTransformationPage
---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const IBMPlex = IBM_Plex_Sans({
+   subsets: ["latin"],
+   weight:['400','500','600','700'],
+   variable:'--font-obm-plex'
+  });
+
+export const metadata: Metadata = {
+  title: "Imaginify",
+  description: "AI-powered by Imaginify",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables:{colorPrimary:'#624cf5'}
+    }}>
+      <html lang="en">
+        <body className={cn('font-IMBPlex antialiased',IBMPlex.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
